e9fe9d37cc22295a9a5c515f730d5dc8b9d22145
Giam <villagiam4@gmail.com>
2022-11-17 11:35:47 -0500

    separe el index en componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+
+const routes:Routes =[
+  {path:'',redirectTo:'index',pathMatch:'full'},
+  {path:'index',component:IndexComponent}
+]
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { TopbarComponent } from './Components/topbar/topbar.component';
+import { IndexComponent } from './Components/index/index.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    TopbarComponent,
+    IndexComponent,
+    
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
